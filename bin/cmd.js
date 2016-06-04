2d8064369e4784543604a6c2fa052e66d0f5e1af
Jayson Harshbarger <hypercubed@gmail.com>
2016-06-04 14:06:46 +0900

    Added tidy-markdown, set as default

--- bin/cmd.js
+++ bin/cmd.js
@@ -6,20 +6,37 @@ var opts = minimist(process.argv.slice(2), {
   alias: {
     ansi: 'a',
     progress: 'p',
-    markdown: 'm'
+    markdown: 'm',
+    tidy: 't'
   },
   default: {
     ansi: false,
     progress: false,
-    markdown: true
+    markdown: true,
+    tidy: true
   }
 });
 
 var reporter = require('tap-summary/lib/summary')(opts);
 
-process.stdin
-  .pipe(reporter)
-  .pipe(process.stdout);
+process.stdin.on('error', function () {
+  process.exit(1);
+});
+
+if (opts.markdown && opts.tidy) {
+  var tidyMarkdown = require('tidy-markdown');
+  var concat = require('concat-stream');
+
+  process.stdin
+    .pipe(reporter)
+    .pipe(concat(function (data) {
+      process.stdout.write(tidyMarkdown(String(data)));
+    }));
+} else {
+  process.stdin
+    .pipe(reporter)
+    .pipe(process.stdout);
+}
 
 process.on('exit', function (status) {
   if (status === 1) {
